b5098f5b85c27cdc9fb981e16f400174d08974f6
Caleb Wightman <agentcboy@gmail.com>
2024-01-09 04:23:11 +0000

    dumb

--- app/chat.Client/Program.cs
+++ app/chat.Client/Program.cs
@@ -1,3 +1,11 @@
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+/// <inheritdoc/>
+public class Program
+{
+  private static async global::System.Threading.Tasks.Task Main(string[] args)
+  {
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+    await builder.Build().RunAsync();
+  }
+}
