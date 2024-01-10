b5dcb5c886942f2ff442ed44b488566ed8b5a3e6
Caleb Wightman <agentcboy@gmail.com>
2024-01-10 17:16:23 +0000

    rename and organize project structure

---
+++ src/Chat/Chat.Data/Entities/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Chat.Data.Entities;
+
+public partial class Person
+{
+  public int Id { get; set; }
+
+  public string Name { get; set; } = null!;
+}
---
+++ src/Chat/Chat.Web/Chat.Web/Program.cs
@@ -0,0 +1,43 @@
+using Chat.Data;
+using Chat.Web.Client.Pages;
+using Chat.Web.Components;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ChatDbContext>(options =>
+
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
