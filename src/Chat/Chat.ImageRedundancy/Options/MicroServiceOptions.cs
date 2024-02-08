a2391a366d7a12e0a87a8bae1a8e5736a03e7a7e
Caleb Wightman <agentcboy@gmail.com>
2024-02-08 13:34:20 -0700

    Add image redundancy console app

---
+++ src/Chat/Chat.ImageRedundancy/Options/MicroServiceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Chat.ImageRedundancy.Options;
+
+public class MicroServiceOptions
+{
+    public int SleepInterval { get; set; } = 5;
+
+    public string ImageProcessingServiceName { get; set; } = "imageprocessing";
+
+}
---
+++ src/Chat/Chat.ImageRedundancy/Program.cs
@@ -0,0 +1,27 @@
+﻿using Chat.ImageRedundancy.Options;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // read the configuration from the environment variables
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+        // bind to the MicroServiceOptions
+        MicroServiceOptions microServiceOptions = new();
+        configuration.GetRequiredSection(nameof(MicroServiceOptions)).Bind(microServiceOptions);
+
+
+        while (true)
+        {
+            // sleep for the configured interval
+            Thread.Sleep(microServiceOptions.SleepInterval * 1000);
+
+            // check for redundant images
+            Console.WriteLine("Checking for redundant images");
+        }
+    }
+}
